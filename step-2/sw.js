1ffa41ffa66fcddc483b92d21da6d500f3eb6400
Lalit Umbarkar <lalit.umbarkar9@gmail.com>
2020-11-02 09:00:12 +0530

    Minor update in step-2, as first time offline was not working
    Signed-off-by: Lalit Umbarkar <lalit.umbarkar9@gmail.com>

--- step-2/sw.js
+++ step-2/sw.js
@@ -4,13 +4,18 @@
  */
 
 const CACHE_KEY = "randomKash";
+const staticFiles = ["/potential-pancake/step-2/index.html",
+    "https://cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js",
+    "https://cdn.jsdelivr.net/npm/fomantic-ui@2.8.7/dist/semantic.min.css",
+    "https://cdn.jsdelivr.net/npm/fomantic-ui@2.8.7/dist/semantic.min.js",
+    "https://mrl1605.github.io/potential-pancake/assets/common-styles.css"]
 
 self.addEventListener("install", (e) => {
     console.log("[ServiceWorker]: Installed");
     e.waitUntil(
         caches.open(CACHE_KEY).then((cache) => {
             console.log("[ServiceWorker]: Caching");
-            return cache.addAll(["./index.html"])
+            return cache.addAll(staticFiles);
         })
     );
 });
